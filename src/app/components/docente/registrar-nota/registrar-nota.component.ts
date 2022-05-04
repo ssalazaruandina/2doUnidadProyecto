31f3012296a41c36962de17d61c32c95f23f7f87
ssalazaruandina <019101931k@uandina.edu.pe>
2022-05-04 11:03:00 -0700

    commmit curso

--- src/app/components/docente/registrar-nota/registrar-nota.component.ts
+++ src/app/components/docente/registrar-nota/registrar-nota.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/interfaces/alumno';
+import { alumnos } from 'src/app/interfaces/baseDatos';
 
 @Component({
   selector: 'app-registrar-nota',
@@ -11,5 +13,70 @@ export class RegistrarNotaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public lista:Alumno[] = alumnos;
+  public parcial1:number = 0;
+  public parcial2:number = 0;
+  public parcial3:number = 0;
+  public total: number = 0;
+
+  onPromedio(): number{
+    this.total = 0;
+    this.total =(this.parcial1+this.parcial2+this.parcial3)/3
+    console.log("El promedio es: "+ this.total);
+    return this.total;
+  }
+
+  //Guardar posicion
+  public posicion: number = -1;
+  guardarPos(i: number){
+    this.total = 0;
+    this.posicion = i;
+  }
+  
+  //Guardar notas
+  guardarNota(){
+    this.parcial1 = 0;
+    this.parcial2 = 0;
+    this.parcial3 = 0;
+    this.lista[this.posicion].nota=this.total;
+    console.log("Se guardó la nota");
+  }
+
+  onConfirmar(){
+    alert("Se publicaron las notas");
+  }
+
+  //Buscar alumno
+  public validar:boolean = false;
+  public alumnoBuscado: string = "";
+  public alumnoBuscadoTotal: Alumno = {
+    codigo: "",
+    usuario:"",
+    dni: "",
+    nombre: "",
+    apellido: "",
+    contrasenia: "",
+    nota: 0
+}
+  public numro: number = -1;
+
+  onBuscar(){
+    for(let i=0;i<this.lista.length;i++){
+      if(this.lista[i].codigo==this.alumnoBuscado){
+        console.log("El alumno es " + this.lista[i].nombre)
+        this.validar=true;
+        this.alumnoBuscadoTotal=this.lista[i];
+        this.numro = i;
+        break;
+      }else{
+        this.validar=false;
+      }
+    }
+  }
+
+}
+
+
 
 }
